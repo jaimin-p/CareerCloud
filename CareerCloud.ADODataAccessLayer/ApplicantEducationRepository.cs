d75b62820ccbea23038ca8c618b3fb0efdca4f93
Jaimin <jaimin.ece@gmail.com>
2018-06-13 17:58:31 -0400

    Populated Update Method

--- CareerCloud.ADODataAccessLayer/ApplicantEducationRepository.cs
+++ CareerCloud.ADODataAccessLayer/ApplicantEducationRepository.cs
@@ -90,7 +90,35 @@ namespace CareerCloud.ADODataAccessLayer
 
         public void Update(params ApplicantEducationPoco[] items)
         {
-            throw new NotImplementedException();
+            using (_connection)
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = _connection;
+
+                foreach(ApplicantEducationPoco poco in items)
+                {
+                    cmd.CommandText = @"UPDATE Applicant_Educations 
+                                      SET Applicant = @Applicant,
+                                          Major = @Major,
+                                          Certificate_Diploma = @Certificate_Diploma,
+                                          Start_Date = @Start_Date,
+                                          Completion_Date = @Completion_Date,
+                                          Completion_Percent = @Completion_Percent
+                                            WHERE Id = @Id";
+
+                    cmd.Parameters.AddWithValue("@Applicant", poco.Applicant);
+                    cmd.Parameters.AddWithValue("@Major", poco.Major);
+                    cmd.Parameters.AddWithValue("@Certificate_Diploma", poco.CertificateDiploma);
+                    cmd.Parameters.AddWithValue("@Start_Date", poco.StartDate);
+                    cmd.Parameters.AddWithValue("@Completion_Date", poco.CompletionDate);
+                    cmd.Parameters.AddWithValue("@Completion_Percent", poco.CompletionPercent);
+                    cmd.Parameters.AddWithValue("@Id", poco.Id);
+
+                    _connection.Open();
+                    cmd.ExecuteNonQuery();
+                    _connection.Close();
+                }
+            }
         }
     }
 
