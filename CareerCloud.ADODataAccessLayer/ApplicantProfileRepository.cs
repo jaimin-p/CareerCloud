2b8b7b3ea6d2bb429b723065fd10670b6788b566
Jaimin <jaimin.ece@gmail.com>
2018-06-13 19:11:08 -0400

    Populated all method for ApplicantProfileRepository

--- CareerCloud.ADODataAccessLayer/ApplicantProfileRepository.cs
+++ CareerCloud.ADODataAccessLayer/ApplicantProfileRepository.cs
@@ -6,14 +6,65 @@ using System.Threading.Tasks;
 using CareerCloud.Pocos;
 using CareerCloud.DataAccessLayer;
 using System.Linq.Expressions;
+using System.Data.SqlClient;
 
 namespace CareerCloud.ADODataAccessLayer
 {
-    public class ApplicantProfileRepository : IDataRepository<ApplicantProfilePoco>
+    public class ApplicantProfileRepository : BaseADO , IDataRepository<ApplicantProfilePoco>
     {
         public void Add(params ApplicantProfilePoco[] items)
         {
-            throw new NotImplementedException();
+            SqlConnection _connection = new SqlConnection(_connString);
+            using (_connection)
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = _connection;
+
+                foreach(ApplicantProfilePoco poco in items)
+                {
+                    cmd.CommandText = @"INSERT INTO [dbo].[Applicant_Profiles]
+                                                        (
+                                                           [Id]
+                                                          ,[Login]
+                                                          ,[Current_Salary]
+                                                          ,[Current_Rate]
+                                                          ,[Currency]
+                                                          ,[Country_Code]
+                                                          ,[State_Province_Code]
+                                                          ,[Street_Address]
+                                                          ,[City_Town]
+                                                          ,[Zip_Postal_Code]
+                                                        )
+                                                        VALUES
+                                                        (
+                                                           @Id
+                                                          ,@Login
+                                                          ,@Current_Salary
+                                                          ,@Current_Rate
+                                                          ,@Currency
+                                                          ,@Country_Code
+                                                          ,@State_Province_Code
+                                                          ,@Street_Address
+                                                          ,@City_Town
+                                                          ,@Zip_Postal_Code
+                                                        )";
+
+                    cmd.Parameters.AddWithValue("@Id", poco.Id);
+                    cmd.Parameters.AddWithValue("@Login", poco.Login);
+                    cmd.Parameters.AddWithValue("@Current_Salary", poco.CurrentSalary);
+                    cmd.Parameters.AddWithValue("@Current_Rate", poco.CurrentRate);
+                    cmd.Parameters.AddWithValue("@Currency", poco.Currency);
+                    cmd.Parameters.AddWithValue("@Country_Code", poco.Country);
+                    cmd.Parameters.AddWithValue("@State_Province_Code", poco.Province);
+                    cmd.Parameters.AddWithValue("@Street_Address", poco.Street);
+                    cmd.Parameters.AddWithValue("@City_Town", poco.City);
+                    cmd.Parameters.AddWithValue("@Zip_Postal_Code", poco.PostalCode);
+
+                    _connection.Open();
+                    cmd.ExecuteNonQuery();
+                    _connection.Close();
+                }
+            }
         }
 
         public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
@@ -23,7 +74,39 @@ namespace CareerCloud.ADODataAccessLayer
 
         public IList<ApplicantProfilePoco> GetAll(params Expression<Func<ApplicantProfilePoco, object>>[] navigationProperties)
         {
-            throw new NotImplementedException();
+            ApplicantProfilePoco[] result = new ApplicantProfilePoco[1000];
+            SqlConnection _connection = new SqlConnection(_connString);
+            using (_connection)
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = _connection;
+                cmd.CommandText = "SELECT * from Applicant_Profiles";
+
+                _connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                int p = 0;
+                while (reader.Read())
+                {
+                    ApplicantProfilePoco poco = new ApplicantProfilePoco();
+                    poco.Id = reader.GetGuid(0);
+                    poco.Login = reader.GetGuid(1);
+                    poco.CurrentSalary = reader.IsDBNull(2) ? null : (decimal?)reader.GetDecimal(2);
+                    poco.CurrentRate = reader.IsDBNull(3) ? null : (decimal?)reader.GetDecimal(3);
+                    poco.Currency = reader.IsDBNull(4) ? null : reader.GetString(4);
+                    poco.Country = reader.IsDBNull(5) ? null : reader.GetString(5);
+                    poco.Province = reader.IsDBNull(6) ? null : reader.GetString(6);
+                    poco.Street = reader.IsDBNull(7) ? null : reader.GetString(7);
+                    poco.City = reader.IsDBNull(8) ? null : reader.GetString(8);
+                    poco.PostalCode = reader.IsDBNull(9) ? null : reader.GetString(9);
+                    poco.TimeStamp = (byte[])reader["Time_Stamp"];
+
+                    result[p] = poco;
+                    p++;
+                }
+                _connection.Close();
+            }
+
+            return result.Where(t => t != null).ToList();
         }
 
         public IList<ApplicantProfilePoco> GetList(Expression<Func<ApplicantProfilePoco, bool>> where, params Expression<Func<ApplicantProfilePoco, object>>[] navigationProperties)
@@ -33,17 +116,68 @@ namespace CareerCloud.ADODataAccessLayer
 
         public ApplicantProfilePoco GetSingle(Expression<Func<ApplicantProfilePoco, bool>> where, params Expression<Func<ApplicantProfilePoco, object>>[] navigationProperties)
         {
-            throw new NotImplementedException();
+            IQueryable<ApplicantProfilePoco> pocos = GetAll().AsQueryable();
+            return pocos.Where(where).FirstOrDefault();
         }
 
         public void Remove(params ApplicantProfilePoco[] items)
         {
-            throw new NotImplementedException();
+            SqlConnection _connection = new SqlConnection(_connString);
+            using (_connection)
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = _connection;
+
+                foreach(ApplicantProfilePoco poco in items)
+                {
+                    cmd.CommandText = @"DELETE from Applicant_Profiles WHERE Id = @id";
+                    cmd.Parameters.AddWithValue("@Id", poco.Id);
+
+                    _connection.Open();
+                    cmd.ExecuteNonQuery();
+                    _connection.Close();
+                }
+            }
         }
 
         public void Update(params ApplicantProfilePoco[] items)
         {
-            throw new NotImplementedException();
+            SqlConnection _connection = new SqlConnection(_connString);
+            using (_connection)
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = _connection;
+
+                foreach (ApplicantProfilePoco poco in items)
+                {
+                    cmd.CommandText = @"UPDATE [dbo].[Applicant_Profiles]
+                                                 SET [Login] = @Login
+                                                    ,[Current_Salary] = @Current_Salary
+                                                    ,[Current_Rate] = @Current_Rate
+                                                    ,[Currency] = @Currency
+                                                    ,[Country_Code] = @Country_Code
+                                                    ,[State_Province_Code] = @State_Province_Code
+                                                    ,[Street_Address] = @Street_Address
+                                                    ,[City_Town] = @City_Town
+                                                    ,[Zip_Postal_Code] = @Zip_Postal_Code
+                                               WHERE [Id] = @Id";
+
+                    cmd.Parameters.AddWithValue("@Id", poco.Id);
+                    cmd.Parameters.AddWithValue("@Login", poco.Login);
+                    cmd.Parameters.AddWithValue("@Current_Salary", poco.CurrentSalary);
+                    cmd.Parameters.AddWithValue("@Current_Rate", poco.CurrentRate);
+                    cmd.Parameters.AddWithValue("@Currency", poco.Currency);
+                    cmd.Parameters.AddWithValue("@Country_Code", poco.Country);
+                    cmd.Parameters.AddWithValue("@State_Province_Code", poco.Province);
+                    cmd.Parameters.AddWithValue("@Street_Address", poco.Street);
+                    cmd.Parameters.AddWithValue("@City_Town", poco.City);
+                    cmd.Parameters.AddWithValue("@Zip_Postal_Code", poco.PostalCode);
+
+                    _connection.Open();
+                    cmd.ExecuteNonQuery();
+                    _connection.Close();
+                }
+            }
         }
     }
 }
