8f200028c7794fe2591afd705dac884a97a1e674
Jaimin <jaimin.ece@gmail.com>
2018-06-14 00:32:16 -0400

    Populated System Country Code repo

--- CareerCloud.ADODataAccessLayer/SystemCountryCodeRepository.cs
+++ CareerCloud.ADODataAccessLayer/SystemCountryCodeRepository.cs
@@ -6,26 +6,73 @@ using System.Threading.Tasks;
 using CareerCloud.Pocos;
 using CareerCloud.DataAccessLayer;
 using System.Linq.Expressions;
+using System.Data.SqlClient;
 
 namespace CareerCloud.ADODataAccessLayer
 {
-    public class SystemCountryCodeRepository : IDataRepository<SystemCountryCodePoco>
+    public class SystemCountryCodeRepository : BaseADO , IDataRepository<SystemCountryCodePoco>
     {
         public void Add(params SystemCountryCodePoco[] items)
         {
-            throw new NotImplementedException();
+            SqlConnection _connection = new SqlConnection(_connString);
+            using (_connection)
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = _connection;
+
+                foreach(SystemCountryCodePoco poco in items)
+                {
+                    cmd.CommandText = @"INSERT INTO [dbo].[System_Country_Codes]
+                                                        (
+                                                           [Code]
+                                                          ,[Name]
+                                                        )
+                                                        VALUES
+                                                        (
+                                                           @Code
+                                                          ,@Name
+                                                        )";
+
+                    cmd.Parameters.AddWithValue("@Code", poco.Code);
+                    cmd.Parameters.AddWithValue("@Name", poco.Name);
+
+                    _connection.Open();
+                    cmd.ExecuteNonQuery();
+                    _connection.Close();
+                }
+            }
         }
 
-        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+            public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
         {
             throw new NotImplementedException();
         }
 
         public IList<SystemCountryCodePoco> GetAll(params Expression<Func<SystemCountryCodePoco, object>>[] navigationProperties)
         {
-            throw new NotImplementedException();
-        }
+            SystemCountryCodePoco[] result = new SystemCountryCodePoco[1000];
+            SqlConnection _connection = new SqlConnection(_connString);
+            using (_connection)
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = _connection;
+                cmd.CommandText = "SELECT * from System_Country_Codes";
 
+                _connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                int p = 0;
+                while (reader.Read())
+                {
+                    SystemCountryCodePoco poco = new SystemCountryCodePoco();
+                    poco.Code = reader.GetString(0);
+                    poco.Name = reader.GetString(1);
+                    result[p] = poco;
+                    p++;
+                }
+                _connection.Close();
+            }
+            return result.Where(t => t != null).ToList();
+        }
         public IList<SystemCountryCodePoco> GetList(Expression<Func<SystemCountryCodePoco, bool>> where, params Expression<Func<SystemCountryCodePoco, object>>[] navigationProperties)
         {
             throw new NotImplementedException();
@@ -33,17 +80,54 @@ namespace CareerCloud.ADODataAccessLayer
 
         public SystemCountryCodePoco GetSingle(Expression<Func<SystemCountryCodePoco, bool>> where, params Expression<Func<SystemCountryCodePoco, object>>[] navigationProperties)
         {
-            throw new NotImplementedException();
+            IQueryable<SystemCountryCodePoco> pocos = GetAll().AsQueryable();
+            return pocos.Where(where).FirstOrDefault();
         }
 
         public void Remove(params SystemCountryCodePoco[] items)
         {
-            throw new NotImplementedException();
+            SqlConnection _connection = new SqlConnection(_connString);
+            using (_connection)
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = _connection;
+
+                foreach (SystemCountryCodePoco poco in items)
+                {
+                    cmd.CommandText = @"DELETE FROM [dbo].[System_Country_Codes]
+                                                      WHERE [Id] = @Id";
+
+                    cmd.Parameters.AddWithValue("@Id", poco.Id);
+
+                    _connection.Open();
+                    cmd.ExecuteNonQuery();
+                    _connection.Close();
+                }
+            }
         }
 
         public void Update(params SystemCountryCodePoco[] items)
         {
-            throw new NotImplementedException();
+            SqlConnection _connection = new SqlConnection(_connString);
+            using (_connection)
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = _connection;
+
+                foreach(SystemCountryCodePoco poco in items)
+                {
+                    cmd.CommandText = @"UPDATE [dbo].[System_Country_Codes]
+                                                 SET [Name] = @Name
+                                               WHERE [Code] = @Code";
+
+                    cmd.Parameters.AddWithValue("@Code", poco.Code);
+                    cmd.Parameters.AddWithValue("@Name", poco.Name);
+
+                    _connection.Open();
+                    cmd.ExecuteNonQuery();
+                    _connection.Close();
+                }
+            }
         }
-    }
+     }
 }
