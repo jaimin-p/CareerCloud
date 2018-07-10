b73e0ad383d9469891b9abbff6a5dc0216c3c65f
Jaimin <jaimin.ece@gmail.com>
2018-07-10 12:16:28 -0400

    Added ApplicantEducationLogic and populated to verify the rules

--- CareerCloud.BusinessLogicLayer/ApplicantEducationLogic.cs
+++ CareerCloud.BusinessLogicLayer/ApplicantEducationLogic.cs
@@ -13,6 +13,38 @@ namespace CareerCloud.BusinessLogicLayer
         public ApplicantEducationLogic(IDataRepository<ApplicantEducationPoco> repository) : base(repository)
         { }
 
+        public override void Add(ApplicantEducationPoco[] pocos)
+        {
+            base.Add(pocos);
+        }
 
+        public override void Update(ApplicantEducationPoco[] pocos)
+        {
+            base.Update(pocos);
+        }
+
+        protected override void Verify(ApplicantEducationPoco[] pocos)
+        {
+            List<ValidationException> validationErrors = new List<ValidationException>();
+
+            foreach(ApplicantEducationPoco poco in pocos)
+            {
+                if (string.IsNullOrEmpty(poco.Major))
+                    validationErrors.Add(new ValidationException(107, $"Major for ApplicantEducation {poco.Major} cannot be empty"));
+                else if (poco.Major.Length < 3)
+                    validationErrors.Add(new ValidationException(107,$"Major for ApplicantEducation {poco.Major} cannot be less than 3 characters "));
+
+                if (poco.StartDate > DateTime.Today)
+                    validationErrors.Add(new ValidationException(108,$"StartDate for ApplicantEducation {poco.StartDate} cannot be Greater than Today"));
+
+                if (poco.CompletionDate < poco.StartDate)
+                    validationErrors.Add(new ValidationException(109,$"CompletionDate for ApplicantEducation {poco.CompletionDate} cannot be earlier than StartDate {poco.StartDate} "));
+            }
+
+            if(validationErrors.Count > 0)
+            {
+                throw new AggregateException(validationErrors);
+            }
+        }
     }
 }
