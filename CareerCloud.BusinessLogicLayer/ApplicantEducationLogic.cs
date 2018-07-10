35df6b3a555b97da763e93652d2b817d26d53441
Jaimin <jaimin.ece@gmail.com>
2018-07-10 11:44:50 -0400

    Added Business Login Layer  , Created Validation Exception and BaseLogic

---
+++ CareerCloud.BusinessLogicLayer/ApplicantEducationLogic.cs
@@ -0,0 +1,14 @@
+﻿using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class ApplicantEducationLogic :BaseLogic<ApplicantEducationPoco>
+    {
+        
+    }
+}
