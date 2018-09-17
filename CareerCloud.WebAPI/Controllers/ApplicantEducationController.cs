a633381aec4a2226b08ac5e1fa9e0635fd4dff99
jaimin <jaimin.ece@gmail.com>
2018-09-17 10:50:57 -0400

    Created WebAPI Project and Added ApplicantEducation Controller Class

---
+++ CareerCloud.WebAPI/Controllers/ApplicantEducationController.cs
@@ -0,0 +1,24 @@
+﻿using CareerCloud.BusinessLogicLayer;
+using CareerCloud.EntityFrameworkDataAccess;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CareerCloud.WebAPI.Controllers
+{
+    public class ApplicantEducationController : ApiController
+    {
+        private ApplicantEducationLogic _logic;
+
+        public ApplicantEducationController()
+        {
+            var repository = new EFGenericRepository<ApplicantEducationPoco>(false);
+            _logic = new ApplicantEducationLogic(repository);
+        }
+
+    }
+}
